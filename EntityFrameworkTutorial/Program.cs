c961bcae73632b67237853ea2ec687c4b15a3762
max-student <max-student@maxtrain.com>
2021-02-26 12:24:58 -0500

    commit

--- EntityFrameworkTutorial/Program.cs
+++ EntityFrameworkTutorial/Program.cs
@@ -5,14 +5,57 @@ using System.Linq;
 namespace EntityFrameworkTutorial {
     class Program {
         static void Main(string[] args) {
-            //create instance of context
-            var _context = new eddbContext();
 
-            //getting all Students firstname and lastname
+            //Get ALL
+            var stcrl = new StudentsController();
+            var students = stcrl.GetAll();
+            foreach (var s in students) {
+                Console.WriteLine($"{s.Firstname} {s.Lastname}");
+
+            }
+
+            //Get by Primary Key
+            var spk = stcrl.GetByPk(1);
+                if(spk == null){
+                Console.WriteLine("Not Found");
+            } else {
+                Console.WriteLine($"{spk.Firstname} {spk.Lastname}");
+            }
+
+            //Insert / Create
+            var sAndrea = new Student {
+                Id = 0, Firstname = "Andrea", Lastname = "Davis", StateCode = "OH",
+                Gpa = 3.0m, Sat = 1500, MajorId = 1
+            };
+            var sAndreaNew = stcrl.Create(sAndrea);
+             Console.WriteLine($"{sAndreaNew.Id} {sAndreaNew.Firstname } {sAndreaNew.Lastname}");
+            
+
+            
+
+
+
+
+
+
+        }
+
+        
+        
+        
+        
+        
+        static void Run1() {
+
+
+            // create instance of context
+            //var _context = new eddbContext();
+
+            // getting all Students firstname and lastname
             //easier way to get all Student names add context statement in foreach loop
             //var students = _context.Students.ToList();
             //foreach(var s in _context.Students.ToList()) {
-            //  Console.WriteLine($" {s.Firstname} {s.Lastname}");
+            //Console.WriteLine($" {s.Firstname} {s.Lastname}");
             //} 
 
             //using LINQ Method Syntax with Lambda 
@@ -22,24 +65,24 @@ namespace EntityFrameworkTutorial {
             //  .ForEach(s => Console.WriteLine($" {s.Firstname} {s.Lastname}"));
 
             //USING LINQ QUERY SYNTAX
-           // var majors = from m in _context.Majors
-                         //where m.MinSat > 1000
-                         //orderby m.Description
-                         //select m;
+            // var majors = from m in _context.Majors
+            //where m.MinSat > 1000
+            //orderby m.Description
+            //select m;
             //foreach (var m in majors) {
-                //Console.WriteLine($"{m.Description}|{m.MinSat}");
-           // }
-        
+            //Console.WriteLine($"{m.Description}|{m.MinSat}");
+            // }
+
 
             //CLASS EXERCISE-- Join students and Majors. Print Name and Major (inner join)
-            var allstudents = (from s in _context.Students
-                              join m in _context.Majors
-                              on s.MajorId equals m.Id
-                              select new {
-                                  Name = s.Firstname + " " + s.Lastname,
-                                  Major = s.MajorId == null ? "Undeclared" : m.Description
-                              }).ToList();
-                  allstudents.ForEach(s => Console.WriteLine($"{s.Name}- {s.Major}"));
+            //var allstudents = (from s in _context.Students
+            //join m in _context.Majors
+            //on s.MajorId equals m.Id
+            //select new {
+            // Name = s.Firstname + " " + s.Lastname,
+            // Major = s.MajorId == null ? "Undeclared" : m.Description
+            //}).ToList();
+            //allstudents.ForEach(s => Console.WriteLine($"{s.Name}- {s.Major}"));
 
 
 
@@ -49,5 +92,8 @@ namespace EntityFrameworkTutorial {
 
 
 
+
+
     }
 }
+
---
+++ EntityFrameworkTutorial/StudentsController.cs
@@ -0,0 +1,49 @@
+﻿using EntityFrameworkTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityFrameworkTutorial {
+    public class StudentsController {
+
+        private readonly eddbContext _context;
+
+        //Get All Method
+        public IEnumerable<Student> GetAll() {
+            //pick collection
+           return _context.Students.ToList();
+        }
+
+        //Get by Primary Key
+        public Student GetByPk(int id) {
+            return _context.Students.Find(id);
+        }
+
+        //Insert (Create)
+
+        public Student Create(Student student) {
+            if (student == null) {
+                throw new Exception("Student cannot be null!");
+            }
+            if (student.Id != 0) {
+                throw new Exception("Student.Id must be zero!");
+            }
+            _context.Students.Add(student);
+            var rowsAffected = _context.SaveChanges();
+            if (rowsAffected != 1) {
+                throw new Exception("Insert/Create failed");
+            }
+            return student;
+        } 
+        
+
+        //Default Construtor no parameter
+        public StudentsController() {
+            _context = new eddbContext();
+
+        }
+
+
+    }
+}
